c9a8b6716e1ad9870322cf48cf826f37e50ce7db
dreamthreebs <wdcvbnmk@outlook.com>
2024-01-09 10:39:28 +0800

    fit still by lon lat but the model is continuous

---
+++ psfit/newfit/do_ps_ns.py
@@ -0,0 +1,98 @@
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+import pickle
+
+from iminuit import Minuit
+from iminuit.cost import LeastSquares
+from numpy.polynomial.legendre import Legendre
+from scipy.interpolate import CubicSpline
+
+# from fit_ps_base import FitPointSource
+# from fitv1 import FitPointSource
+# from fitv2 import FitPointSource
+# from discrete_fit1 import FitPointSource
+from fit_ps_ns import FitPointSource
+
+def main():
+    m = np.load('../../FGSim/PSNOISE/2048/40.npy')[0]
+    nstd = np.load('../../FGSim/NSTDNORTH/2048/40.npy')[0]
+    df_mask = pd.read_csv('../partial_sky_ps/ps_in_mask/mask40.csv')
+    df_ps = pd.read_csv('../../test/ps_sort/sort_by_iflux/40.csv')
+
+    lmax = 350
+    nside = 2048
+    beam = 63
+    bl = hp.gauss_beam(fwhm=np.deg2rad(beam)/60, lmax=lmax)
+    cl_cmb = np.load('../../src/cmbsim/cmbdata/cmbcl.npy')[:lmax+1,0]
+    cl_cmb = cl_cmb * bl**2
+
+    flux_idx = 0
+    lon = np.rad2deg(df_mask.at[flux_idx, 'lon'])
+    lat = np.rad2deg(df_mask.at[flux_idx, 'lat'])
+    iflux = df_mask.at[flux_idx, 'iflux']
+
+    obj = FitPointSource(m=m, nstd=nstd, flux_idx=flux_idx, df_mask=df_mask, df_ps=df_ps, cl_cmb=cl_cmb, lon=lon, lat=lat, iflux=iflux, lmax=lmax, nside=nside, radius_factor=1.0, beam=beam)
+
+    obj.see_true_map(m=m, lon=lon, lat=lat, nside=nside, beam=beam)
+    # obj.find_nearby_ps_lon_lat()
+    # obj.find_first_second_nearby_ps_lon_lat()
+    # obj.fit_ps_ns(mode='10params')
+    norm_beam, fit_lon, fit_lat =  obj.fit_ps_ns()
+    print(f'{norm_beam=}, {fit_lon=}, {fit_lat=}')
+
+def main1():
+    m = np.load('../../FGSim/FITDATA/PSNOISE/40.npy')[0]
+    nstd = np.load('../../FGSim/NSTDNORTH/2048/40.npy')[0]
+    df_mask = pd.read_csv('../partial_sky_ps/ps_in_mask/mask40.csv')
+    df_ps = pd.read_csv('../../test/ps_sort/sort_by_iflux/40.csv')
+
+    lmax = 350
+    nside = 2048
+    beam = 63
+    bl = hp.gauss_beam(fwhm=np.deg2rad(beam)/60, lmax=lmax)
+    cl_cmb = np.load('../../src/cmbsim/cmbdata/cmbcl.npy')[:lmax+1,0]
+    cl_cmb = cl_cmb * bl**2
+    chi2dof_list = []
+    norm_beam_list = []
+    norm_error_list = []
+    fit_lon_list = []
+    fit_lat_list = []
+    num_ps_list = []
+
+
+    for flux_idx in range(0,139):
+        print(f'{flux_idx=}')
+        lon = np.rad2deg(df_mask.at[flux_idx, 'lon'])
+        lat = np.rad2deg(df_mask.at[flux_idx, 'lat'])
+        iflux = df_mask.at[flux_idx, 'iflux']
+
+        obj = FitPointSource(m=m, nstd=nstd, flux_idx=flux_idx, df_mask=df_mask, df_ps=df_ps, cl_cmb=cl_cmb, lon=lon, lat=lat, iflux=iflux, lmax=lmax, nside=nside, radius_factor=1.0, beam=beam)
+
+        # obj.see_true_map(m=m, lon=lon, lat=lat, nside=nside, beam=beam)
+        # obj.find_nearby_ps_lon_lat()
+        # obj.find_first_second_nearby_ps_lon_lat()
+        # obj.fit_ps_ns(mode='10params')
+        num_ps, chi2dof, norm_beam, norm_error, fit_lon, fit_lat =  obj.fit_ps_ns()
+        # print(f'{norm_beam=}, {norm_error=}, {fit_lon=}, {fit_lat=}, {chi2dof=}')
+        num_ps_list.append(num_ps)
+        chi2dof_list.append(chi2dof)
+        norm_beam_list.append(norm_beam)
+        norm_error_list.append(norm_error)
+        fit_lon_list.append(fit_lon)
+        fit_lat_list.append(fit_lat)
+    np.save("./ps_ns_data/num_ps.npy", np.array(num_ps_list))
+    np.save("./ps_ns_data/chi2dof1.npy", np.array(chi2dof_list))
+    np.save("./ps_ns_data/norm_beam1.npy",np.array(norm_beam_list))
+    np.save("./ps_ns_data/norm_error1.npy",np.array(norm_error_list))
+    np.save("./ps_ns_data/fit_lon1.npy",np.array(fit_lon_list))
+    np.save("./ps_ns_data/fit_lat1.npy",np.array(fit_lat_list))
+
+
+if __name__ == '__main__':
+    main1()
+    # main()
+
+
---
+++ psfit/newfit/ps_ns_data/load.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import healpy as hp
+import matplotlib.pyplot as plt
+
+norm_beam = np.load('./norm_beam1.npy')
+lon = np.load('./fit_lon1.npy')
+lat = np.load('./fit_lat1.npy')
+norm_error = np.load('./norm_error1.npy')
+chi2dof = np.load('./chi2dof1.npy')
+num_ps = np.load('./num_ps.npy')
+print(f'{norm_beam.shape=}')
+print(f'{norm_beam=}')
+
+def flux2norm_beam(flux):
+    # from mJy to muK_CMB to norm_beam
+    coeffmJy2norm = 2.1198465131100624e-05
+    return coeffmJy2norm * flux
+
+df = pd.read_csv('../../partial_sky_ps/ps_with_nearby/40.csv')
+norm_beam_true = flux2norm_beam(df["iflux"])
+norm_beam_true = norm_beam_true.to_numpy()
+
+norm_beam_all = np.zeros_like(norm_beam_true)
+norm_beam_all[:norm_beam.shape[0]] = norm_beam
+lon_all = np.zeros_like(norm_beam_true)
+lon_all[:lon.shape[0]] = lon
+lat_all = np.zeros_like(norm_beam_true)
+lat_all[:lat.shape[0]] = lat
+norm_error_all = np.zeros_like(norm_beam_true)
+norm_error_all[:norm_error.shape[0]] = norm_error
+chi2dof_all = np.zeros_like(norm_beam_true)
+chi2dof_all[:chi2dof.shape[0]] = chi2dof
+num_ps_all = np.zeros_like(norm_beam_true)
+num_ps_all[:num_ps.shape[0]] = num_ps
+
+
+
+fit_error = np.abs(norm_beam_all - norm_beam_true) / norm_beam_true
+fit_error = np.where(fit_error != 1.0, fit_error, 0)
+
+df["fit_norm"] = norm_beam_all
+df["fit_error"] = fit_error
+df["fit_lon"] = lon_all
+df["fit_lat"] = lat_all
+df["norm_error"] = norm_error_all
+df["chi2dof"] = chi2dof_all
+df["num_ps"] = num_ps_all
+
+df.to_csv(f'./data/40.csv', index=False)
+
