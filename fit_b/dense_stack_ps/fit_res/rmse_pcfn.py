d28d0f6e088828ff340aeb135d9149f7c2003a0f
dreamthreebs <wdcvbnmk@outlook.com>
2024-08-30 10:07:42 +0800

    calc pcfn power spectrum

--- fit_b/dense_stack_ps/fit_res/rmse_pcfn.py
+++ fit_b/dense_stack_ps/fit_res/rmse_pcfn.py
@@ -20,18 +20,18 @@ print(f'{freq=}, {beam=}')
 
 # rmv_list = []
 # rmv1_list = []
-c_list = []
-cn_list = []
-pcn_list = []
+cf_list = []
+cfn_list = []
+pcfn_list = []
 
 rmv_qu_list = []
 rmv_lon_lat_list = []
 # rmv_b_qu_list = []
 
-# rmv1_list = []
-c_qu_list = []
-cn_qu_list = []
-pcn_qu_list = []
+# # rmv1_list = []
+# cf_qu_list = []
+# cfn_qu_list = []
+# pcfn_qu_list = []
 
 ps_mask_list = []
 qu_mask_list = []
@@ -67,22 +67,22 @@ for rlz_idx in range(0,200):
     # n_ps_mask = np.load(f'./pcn_dl/PS_MASK_n/ps_3sigma/{rlz_idx}.npy')
     n_qu_mask = np.load(f'./pcn_dl/ACT/curl_yp_ps_n/{rlz_idx}.npy')
     # rmv = np.load(f'./pcn_dl/QU/removal_3sigma/{rlz_idx}.npy') - n_rmv
-    rmv_qu = np.load(f'./pcn_dl/QU/removal_3sigma/{rlz_idx}.npy') - n_rmv
+    rmv_qu = np.load(f'./pcfn_dl/QU/removal_3sigma/{rlz_idx}.npy') - n_rmv
     # rmv_lon_lat = np.load(f'./pcn_dl/QU_lon_lat/removal_3sigma/{rlz_idx}.npy') - n_lon_lat
     # rmv_b_qu = np.load(f'./pcn_dl/QU_B/removal_3sigma/{rlz_idx}.npy') - n_qu
     # rmv1 = np.load(f'./pcn_dl/B/removal_10sigma/{rlz_idx}.npy')
-    c = np.load(f'./pcn_dl/QU/c/{rlz_idx}.npy')
+    cf = np.load(f'./pcfn_dl/QU/cf/{rlz_idx}.npy')
     # c_qu = np.load(f'./pcn_dl/QU/c/{rlz_idx}.npy')
-    cn = np.load(f'./pcn_dl/QU/cn/{rlz_idx}.npy') - n_qu
+    cfn = np.load(f'./pcfn_dl/QU/cfn/{rlz_idx}.npy') - n_qu
     # cn_qu = np.load(f'./pcn_dl/QU/cn/{rlz_idx}.npy') - n_qu
-    pcn = np.load(f'./pcn_dl/QU/pcn/{rlz_idx}.npy') - n_qu
+    pcfn = np.load(f'./pcfn_dl/QU/pcfn/{rlz_idx}.npy') - n_qu
     # pcn_qu = np.load(f'./pcn_dl/QU/pcn/{rlz_idx}.npy') - n_qu
     # ps_mask = np.load(f'./pcn_dl/PS_MASK/ps_3sigma/{rlz_idx}.npy') - n_qu
-    qu_mask = np.load(f'./pcn_dl/ACT/curl_yp_ps/{rlz_idx}.npy') - n_qu_mask
+    qu_mask = np.load(f'./pcfn_dl/ACT/curl_yp_ps/{rlz_idx}.npy') - n_qu_mask
     # inp_qu = np.load(f'./pcn_dl/INP_QU_1/inpaint_qu_3sigma/{rlz_idx}.npy') - n_qu
-    inp_eb = np.load(f'./pcn_dl/INP_B/inpaint_eb_3sigma/{rlz_idx}.npy') - n_inp
+    inp_eb = np.load(f'./pcfn_dl/INP_B/inpaint_eb_3sigma/{rlz_idx}.npy') - n_inp
 
-    # plt.plot(ell_arr, c, label=f'c {rlz_idx}')
+    # plt.plot(ell_arr, cf, label=f'cf {rlz_idx}')
     # plt.plot(ell_arr, n_rmv, label=f'rmv n{rlz_idx}')
     # plt.plot(ell_arr, n_qu, label=f'n {rlz_idx}')
     # plt.plot(ell_arr, rmv_qu, label=f'rmv_qu {rlz_idx}')
@@ -91,19 +91,19 @@ for rlz_idx in range(0,200):
     # # plt.plot(ell_arr, n_ps_mask, label=f'ps mask n {rlz_idx}')
     # plt.plot(ell_arr, n_qu_mask, label=f'qu mask n {rlz_idx}')
     # # plt.plot(ell_arr, n_lon_lat, label=f'rmv lon lat n {rlz_idx}')
-    # plt.plot(ell_arr, np.abs(n_rmv - n_qu), label=f'rmv - n {rlz_idx}')
+    # # plt.plot(ell_arr, np.abs(n_rmv - n_qu), label=f'rmv - n {rlz_idx}')
     # # plt.plot(ell_arr, np.abs(n_lon_lat - n_qu), label=f'rmv lon lat - n {rlz_idx}')
-    # plt.plot(ell_arr, np.abs(n_inp - n_qu), label=f'inp - n {rlz_idx}')
+    # # plt.plot(ell_arr, np.abs(n_inp - n_qu), label=f'inp - n {rlz_idx}')
     # # plt.plot(ell_arr, np.abs(n_ps_mask - n_qu), label=f'ps mask - n {rlz_idx}')
-    # plt.plot(ell_arr, np.abs(n_qu_mask - n_qu), label=f'qu mask - n {rlz_idx}')
+    # # plt.plot(ell_arr, np.abs(n_qu_mask - n_qu), label=f'qu mask - n {rlz_idx}')
     # plt.semilogy()
     # plt.legend()
     # plt.show()
 
     # rmv_list.append(rmv)
-    c_list.append(c)
-    cn_list.append(cn)
-    pcn_list.append(pcn)
+    cf_list.append(cf)
+    cfn_list.append(cfn)
+    pcfn_list.append(pcfn)
 
     rmv_qu_list.append(rmv_qu)
     # rmv_lon_lat_list.append(rmv_lon_lat)
@@ -121,9 +121,9 @@ for rlz_idx in range(0,200):
 
 nsim = 201
 # rmv_arr = np.array(rmv_list)
-c_arr = np.array(c_list)
-cn_arr = np.array(cn_list)
-pcn_arr = np.array(pcn_list)
+cf_arr = np.array(cf_list)
+cfn_arr = np.array(cfn_list)
+pcfn_arr = np.array(pcfn_list)
 
 rmv_qu_arr = np.array(rmv_qu_list)
 # rmv_lon_lat_arr = np.array(rmv_lon_lat_list)
@@ -140,35 +140,35 @@ inp_eb_arr = np.array(inp_eb_list)
 # print(f'{rmv_arr.shape=}')
 
 
-pcn_rmse = np.sqrt(np.sum((pcn_arr-c_arr) ** 2, axis=0) / nsim)
-print(f'{pcn_rmse.shape=}')
-cn_rmse = np.sqrt(np.sum((cn_arr-c_arr) ** 2, axis=0) / nsim)
-rmv_qu_rmse = np.sqrt(np.sum((rmv_qu_arr-c_arr) ** 2, axis=0) / nsim)
+pcfn_rmse = np.sqrt(np.sum((pcfn_arr-cf_arr) ** 2, axis=0) / nsim)
+print(f'{pcfn_rmse.shape=}')
+cfn_rmse = np.sqrt(np.sum((cfn_arr-cf_arr) ** 2, axis=0) / nsim)
+rmv_qu_rmse = np.sqrt(np.sum((rmv_qu_arr-cf_arr) ** 2, axis=0) / nsim)
 # rmv_lon_lat_rmse = np.sqrt(np.sum((rmv_lon_lat_arr-c_arr) ** 2, axis=0) / nsim)
-inp_eb_rmse = np.sqrt(np.sum((inp_eb_arr-c_arr) ** 2, axis=0) / nsim)
+inp_eb_rmse = np.sqrt(np.sum((inp_eb_arr-cf_arr) ** 2, axis=0) / nsim)
 # ps_mask_rmse = np.sqrt(np.sum((ps_mask_arr-c_arr) ** 2, axis=0) / nsim)
-qu_mask_rmse = np.sqrt(np.sum((qu_mask_arr-c_arr) ** 2, axis=0) / nsim)
+qu_mask_rmse = np.sqrt(np.sum((qu_mask_arr-cf_arr) ** 2, axis=0) / nsim)
 
-c_mean = np.mean(c_arr, axis=0)
+cf_mean = np.mean(cf_arr, axis=0)
 print(f'{ell_arr[1:7]=}')
-pcn_rmse_ratio = np.sum(pcn_rmse[1:7] / c_mean[1:7])
-cn_rmse_ratio = np.sum(cn_rmse[1:7] / c_mean[1:7])
-rmv_qu_rmse_ratio = np.sum(rmv_qu_rmse[1:7] / c_mean[1:7])
+pcfn_rmse_ratio = np.sum(pcfn_rmse[1:7] / cf_mean[1:7])
+cfn_rmse_ratio = np.sum(cfn_rmse[1:7] / cf_mean[1:7])
+rmv_qu_rmse_ratio = np.sum(rmv_qu_rmse[1:7] / cf_mean[1:7])
 # rmv_lon_lat_rmse_ratio = np.sum(rmv_lon_lat_rmse[1:7] / c_mean[1:7])
-inp_eb_rmse_ratio = np.sum(inp_eb_rmse[1:7] / c_mean[1:7])
+inp_eb_rmse_ratio = np.sum(inp_eb_rmse[1:7] / cf_mean[1:7])
 # ps_mask_rmse_ratio = np.sum(ps_mask_rmse[1:7] / c_mean[1:7])
-qu_mask_rmse_ratio = np.sum(qu_mask_rmse[1:7] / c_mean[1:7])
-print(f'{pcn_rmse_ratio=}')
-print(f'{cn_rmse_ratio=}')
+qu_mask_rmse_ratio = np.sum(qu_mask_rmse[1:7] / cf_mean[1:7])
+print(f'{pcfn_rmse_ratio=}')
+print(f'{cfn_rmse_ratio=}')
 print(f'{rmv_qu_rmse_ratio=}')
 # print(f'{rmv_lon_lat_rmse_ratio=}')
 print(f'{inp_eb_rmse_ratio=}')
 # print(f'{ps_mask_rmse_ratio=}')
 print(f'{qu_mask_rmse_ratio=}')
 
-plt.scatter(ell_arr, c_mean, label='input CMB power spectrum (not RMSE)', marker='.')
-plt.scatter(ell_arr, pcn_rmse, label='No point source treatment', marker='.')
-plt.scatter(ell_arr, cn_rmse, label='CMB + noise', marker='.')
+plt.scatter(ell_arr, cf_mean, label='input CMB + FG power spectrum (not RMSE)', marker='.')
+plt.scatter(ell_arr, pcfn_rmse, label='No point source treatment', marker='.')
+plt.scatter(ell_arr, cfn_rmse, label='CMB + noise + FG', marker='.')
 plt.scatter(ell_arr, rmv_qu_rmse, label='Template fitting method with fixed lon lat', marker='.')
 # plt.scatter(ell_arr, rmv_lon_lat_rmse, label='template fitting method with free lon lat', marker='.')
 # plt.scatter(ell_arr, rmv_qu_rmse, label='rmv b qu', marker='.')
