8f380b1256fe6212bfbb87826496c6718bb74317
dreamthreebs <wdcvbnmk@outlook.com>
2025-02-20 16:02:58 +0800

    paper: each freq plot

--- fit_b/270GHz/fit_res/paper_plot.py
+++ fit_b/270GHz/fit_res/paper_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import healpy as hp
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import pandas as pd
 import pymaster as nmt
 import glob
@@ -67,19 +68,16 @@ def generate_bins(l_min_start=30, delta_l_min=30, l_max=1500, fold=0.3, l_thresh
     return bins_edges[:-1], bins_edges[1:]
 
 
+# l_min_edges, l_max_edges = generate_bins(l_min_start=30, delta_l_min=30, l_max=lmax+1, fold=0.2)
 l_min_edges, l_max_edges = generate_bins(l_min_start=42, delta_l_min=40, l_max=lmax+1, fold=0.1, l_threshold=400)
 bin_dl = nmt.NmtBin.from_edges(l_min_edges, l_max_edges, is_Dell=True)
-ell_arr = bin_dl.get_effective_ells()
-print(f'{l_min_edges=}, {l_max_edges=}')
-print(f'{ell_arr=}')
-
+# bin_dl = nmt.NmtBin.from_edges(l_min_edges, l_max_edges, is_Dell=True)
 # bin_dl = nmt.NmtBin.from_lmax_linear(lmax=lmax, nlb=40)
-# ell_arr = bin_dl.get_effective_ells()
+ell_arr = bin_dl.get_effective_ells()
 print(f'{ell_arr.shape=}')
-print(f'{ell_arr=}')
 
 def mean_and_std(sim_mode):
-    for rlz_idx in range(1,100):
+    for rlz_idx in range(1,200):
         print(f'{rlz_idx=}')
 
         n_qu = np.load(f'./pcfn_dl4/{sim_mode}/n/{rlz_idx}.npy')
@@ -180,40 +178,93 @@ print(f'{lmax_ell_arr=}')
 cl_cmb = np.load('/afs/ihep.ac.cn/users/w/wangyiming25/work/dc2/psilc/src/cmbsim/cmbdata/cmbcl_8k.npy').T
 print(f'{cl_cmb.shape=}')
 l = np.arange(lmax_eff+1)
+dl_in = bin_dl.bin_cell(cl_cmb[2,:lmax+1])
 
 # Create figure with 2 subplots (main and subfigure), sharing the x-axis
-fig, ax_main = plt.subplots()
+fig, (ax_main, ax_sub) = plt.subplots(2, 1, figsize=(10, 8), sharex=True, gridspec_kw={'height_ratios': [2, 1]})
 
 s = 5
 
 # Set the y-axis to logarithmic scale for both the main plot and subfigure
 ax_main.set_yscale('log')
+ax_sub.set_yscale('log')
 ax_main.set_xscale('log')
-# ax_sub.set_xscale('log')
+ax_sub.set_xscale('log')
 
 
 # Plot mean values in the main axis (no error bars here)
-ax_main.errorbar(ell_arr, cfn_mean[:lmax_ell_arr], yerr=cfn_std[:lmax_ell_arr], label='CMB + FG + NOISE', fmt='.')
-ax_main.errorbar(ell_arr+5, pcfn_mean[:lmax_ell_arr], yerr=pcfn_std[:lmax_ell_arr], label='PS + CMB + FG + NOISE', fmt='.')
-ax_main.errorbar(ell_arr+10, rmv_mean[:lmax_ell_arr], yerr=rmv_std[:lmax_ell_arr], label='Template Fitting method', fmt='.')
-ax_main.errorbar(ell_arr+15, ps_mask_mean[:lmax_ell_arr], yerr=ps_mask_std[:lmax_ell_arr], label='Mask on QU', fmt='.')
-ax_main.errorbar(ell_arr+20, inp_mean[:lmax_ell_arr], yerr=inp_std[:lmax_ell_arr], label='Recycling + Inpaint on B', fmt='.')
-ax_main.plot(l, l*(l+1)*cl_cmb[2,:lmax_eff+1]/(2*np.pi), label='CMB input', color='black')
+ax_main.errorbar(ell_arr*0.99, pcfn_mean[:lmax_ell_arr], yerr=pcfn_std[:lmax_ell_arr], label='PS + CMB + FG + NOISE', fmt='.', color='blue')
+ax_main.errorbar(ell_arr*0.995, cfn_mean[:lmax_ell_arr], yerr=cfn_std[:lmax_ell_arr], label='CMB + FG + NOISE', fmt='.', color='purple')
+ax_main.plot(ell_arr, dl_in[:lmax_ell_arr], label='Fiducial CMB', color='black')
+ax_main.errorbar(ell_arr*1.005, rmv_mean[:lmax_ell_arr], yerr=rmv_std[:lmax_ell_arr], label='Template Fitting method', fmt='.', color='green')
+ax_main.errorbar(ell_arr*1.01, ps_mask_mean[:lmax_ell_arr], yerr=ps_mask_std[:lmax_ell_arr], label='Mask on QU', fmt='.', color='orange')
+ax_main.errorbar(ell_arr*1.015, inp_mean[:lmax_ell_arr], yerr=inp_std[:lmax_ell_arr], label='Recycling + Inpaint on B', fmt='.', color='red')
+# ax_main.plot(l, l*(l+1)*cl_cmb[2,:lmax_eff+1]/(2*np.pi), label='CMB input', color='black')
+
 
 # Set labels and title for the main plot
 ax_main.set_ylabel('$D_\\ell^{BB} [\mu K^2]$')
-ax_main.set_xlim(50, lmax_eff)
+ax_main.set_xlim(58, lmax_eff)
 # ax_main.set_ylim(, lmax_eff)
 ax_main.set_title(f'Debiased power spectra {freq}GHz')
 ax_main.legend()
 
+res_pcfn = np.abs(pcfn_mean - dl_in)
+res_cfn = np.abs(cfn_mean - dl_in)
+res_rmv = np.abs(rmv_mean - dl_in)
+res_inp = np.abs(inp_mean - dl_in)
+res_ps_mask = np.abs(ps_mask_mean - dl_in)
+
+res_line = mlines.Line2D([], [], color='black', linestyle='-', label='Residual')
+std_line = mlines.Line2D([], [], color='black', linestyle=':', label='Std Deviation')
+
+ax_sub.plot(ell_arr, res_pcfn[:lmax_ell_arr], label='PS + CMB + FG + NOISE', marker='.', color='blue')
+ax_sub.plot(ell_arr, res_cfn[:lmax_ell_arr], label='CMB + FG + NOISE', marker='.', color='purple')
+ax_sub.plot(ell_arr, res_rmv[:lmax_ell_arr], label='Template Fitting method', marker='.', color='green')
+ax_sub.plot(ell_arr, res_inp[:lmax_ell_arr], label='Recycling + Inpaint on B', marker='.', color='red')
+ax_sub.plot(ell_arr, res_ps_mask[:lmax_ell_arr], label='Mask on QU', marker='.', color='orange')
+
+# Plot standard deviation in the subfigure (using scatter with no error bars)
+ax_sub.plot(ell_arr, pcfn_std[:lmax_ell_arr], label='PS + CMB + FG + NOISE', color='blue', marker='.', linestyle=':')
+ax_sub.plot(ell_arr, cfn_std[:lmax_ell_arr], label='CMB + FG + NOISE', color='purple', marker='.', linestyle=':')
+ax_sub.plot(ell_arr, rmv_std[:lmax_ell_arr], label='Template Fitting method', color='green', marker='.', linestyle=':')
+ax_sub.plot(ell_arr, ps_mask_std[:lmax_ell_arr], label='Mask on QU', color='orange', marker='.', linestyle=':')
+ax_sub.plot(ell_arr, inp_std[:lmax_ell_arr], label='Recycling + Inpaint on B', color='red', marker='.', linestyle=':')
+
+label_size = 10
+ax_main.tick_params(axis='both', which='major', labelsize=label_size)
+ax_main.tick_params(bottom=True, top=True, left=True, right=True, which = "major", direction="in", length=10, width=2);
+ax_main.tick_params(bottom=True, top=True, left=True, right=True, which = "minor", direction="in", length=5, width=1.5);
+# ax_main.grid(which='major', linestyle='-', linewidth=2)
+# ax_main.grid(which='minor', linestyle='dashed', linewidth=0.9)
+for axis in ['top','bottom','left','right']:
+    ax_main.spines[axis].set_linewidth(3)
+
+ax_sub.tick_params(axis='both', which='major', labelsize=label_size)
+ax_sub.tick_params(bottom=True, top=True, left=True, right=True, which = "major", direction="in", length=10, width=2);
+ax_sub.tick_params(bottom=True, top=True, left=True, right=True, which = "minor", direction="in", length=5, width=1.5);
+# ax_sub.grid(which='major', linestyle='-', linewidth=2)
+# ax_sub.grid(which='minor', linestyle='dashed', linewidth=0.9)
+for axis in ['top','bottom','left','right']:
+    ax_sub.spines[axis].set_linewidth(2)
+
+
+# Set labels for the subfigure (only xlabel here)
+ax_sub.set_xlabel('$\\ell$')
+ax_sub.set_ylabel('Residual and Std Deviation')
+# ax_sub.set_ylim(5e-4, 3e-2)
+    # Set labels for the subfigure (only xlabel here)
+ax_sub.set_xlabel('$\\ell$')
+ax_sub.set_ylabel('Residual and Std Deviation')
+ax_sub.legend(handles=[res_line, std_line])
+
 # Adjust layout for better spacing
 plt.tight_layout()
-# plt.subplots_adjust(hspace=0)
+plt.subplots_adjust(hspace=0)
 
-# path_fig = Path('/afs/ihep.ac.cn/users/w/wangyiming25/tmp/20250131')
-# path_fig.mkdir(exist_ok=True, parents=True)
-# plt.savefig(path_fig / Path(f'{freq}GHz.png'), dpi=300)
+path_fig = Path('/afs/ihep.ac.cn/users/w/wangyiming25/tmp/20250219')
+path_fig.mkdir(exist_ok=True, parents=True)
+plt.savefig(path_fig / Path(f'{freq}GHz.png'), dpi=300)
 
 # Show plot
 plt.show()
